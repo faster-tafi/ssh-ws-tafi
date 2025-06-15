0c0f871b8db73d2f76fa29a7c4345e9d35eca188
faster-tafi <aminetafi588@gmail.com>
2025-06-15 16:12:35 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Setup script to install and configure SSH WS, Xray, UDPGW
+# Author: Tafi 🇲🇦
+
+apt update && apt install -y screen curl wget nginx socat jq net-tools
+
+# [Add here your full setup commands...]
+
+echo "Setup completed!"
